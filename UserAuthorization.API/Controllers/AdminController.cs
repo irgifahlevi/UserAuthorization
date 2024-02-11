4c88fa6be3a8f2a983d7b1f9f1596591ab292e30
irgifahlevi <irgifahlevi5@gmail.com>
2024-02-11 13:47:20 +0700

    Configure swagger adn authorize admin controller

---
+++ UserAuthorization.API/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserAuthorization.API.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class AdminController : ControllerBase
+    {
+
+        private List<string> employee;
+
+        [HttpGet]
+        [Route("GetEmployee")]
+        public IEnumerable<string> GetEmployee()
+        {
+            return employee = new List<string>()
+            {
+                "Jane", "Salman", "Max", "Andrew", "Luccyan", "Matt"
+            };
+        }
+    }
+
+
+}
--- UserAuthorization.API/Program.cs
+++ UserAuthorization.API/Program.cs
@@ -1,18 +1,50 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 using UserAuthorization.API.Entities;
 using UserAuthorization.Facade.Models;
 using UserAuthorization.Facade.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// Konfigurasi Swagger
+builder.Services.AddSwaggerGen( option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "Auth API", Version = "v1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
 
 // Configuration Entity Framework
 var connection = builder.Configuration.GetConnectionString("DB");
@@ -29,7 +61,20 @@ builder.Services.AddAuthentication(o =>
     o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
     o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
     o.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(opt => {
+
+    opt.SaveToken = true;
+    opt.RequireHttpsMetadata = false;
+    opt.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = configuration["JWT:ValidAudience"],
+        ValidIssuer = configuration["JWT:ValidIssuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"])) 
+    };
 });
+  
 
 //Configuration SMTP Email
 var emailConfig = builder.Configuration
@@ -55,6 +100,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
